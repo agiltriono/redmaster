e6a6602028769815b9ee40b05797cc1d1b50ac86
Agil Triono <agiltriono71@yahoo.com>
2016-10-17 21:30:24 +0700

    Update agiel-triono.js

--- js/agiel-triono.js
+++ js/agiel-triono.js
@@ -107,3 +107,39 @@ $(function() {
     }
   };
 })(jQuery);
+
+function smartLink(){
+    this.keywdHref = new Object();
+    this.add = function(keyword, href){
+        if(keyword.substr(0,1) != &quot; &quot;){keyword = &quot; &quot; + keyword;}
+        this.keywdHref[keyword] =  href;
+    }
+    this.createAnchor = function(){
+        var objs = document.getElementsByTagName(&quot;div&quot;);
+        for(var i=0; i&lt;objs.length; i++){
+            var obj = objs[i];
+            if(obj.className.indexOf(&quot;post-body&quot;)&gt;-1){
+                var content = obj.innerHTML;
+                for(var keyword in this.keywdHref){
+                    var href = this.keywdHref[keyword];
+                    var newstr = content.replace(keyword, &quot;&lt;a href=&#39;&quot;+href+&quot;&#39; rel=&#39;nofollow&#39; target=&#39;_blank&#39; title=&#39;&quot;+keyword+&quot;&#39;&gt;&quot;+keyword+&quot;&lt;/a&gt;&quot;, &quot;gi&quot;);
+                    obj.innerHTML = newstr;
+                    content = newstr;
+                }
+            }
+        }
+    }
+    this.startScript = function(){
+        var onLoad = window.onload;
+        window.onload = function(){
+            if(onLoad){onLoad();}
+            setTimeout(&quot;f.createAnchor()&quot;, 100);
+        }
+    }
+}
+var f = new smartLink();
+f.add("Copyright", "https://agieltriono.blogspot.com/");
+f.add("SEO", "https://agieltriono.blogspot.com/");
+f.add("Agiel Triono", "https://agieltriono.blogspot.com/");
+f.add("Gratis", "https://agieltriono.blogspot.com/");
+f.startScript();
